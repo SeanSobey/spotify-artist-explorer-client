b080752f0de671e1f60f6a2e7dc25d3a977abeec
Sean Sobey SAHL <seans@sahomeloans.com>
2019-05-30 11:29:11 +0200

    Updated redirectURI

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const spotify = {
-	redirectURI: 'http://localhost:8080/auth.html',
+	redirectURI: 'https://seansobey.github.io/spotify-artist-explorer-client/auth.html',
+	// redirectURI: 'http://localhost:8080/auth.html',
 	clientId: '46ed660bda484776984c3c614bc14eca',
 };
 export const authServer = 'http://localhost:3000';
